77e15be76dfcbd801af28401aa46f840283f7dee
Davidfhk <davidign1688@hotmail.com>
2017-08-06 14:16:08 +0200

    Creamos la ruta para modificar un usuario

--- src/routes.php
+++ src/routes.php
@@ -10,3 +10,5 @@ $app->get('/users', 'UserController:getAll');
 $app->get('/user/{id}', 'UserController:getUserForId');
 
 $app->post('/user/new', 'UserController:addUser');
+
+$app->put('/user/update/{id}', 'UserController:putUser');
